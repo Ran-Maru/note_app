6eb3b7eb1f0bf60e82d372a32edad22e79c3ea3d
Ran-Maru <63487316+Ran-Maru@users.noreply.github.com>
2021-10-03 15:29:41 +0900

    update noteコンポーネントを作成

--- frontend/main_page/src/App.vue
+++ frontend/main_page/src/App.vue
@@ -5,19 +5,28 @@
     内容
     <input type="text" v-model='content'>
     <button @click='postNote(title, content)'>メモ作成</button>
-    <p>
-      {{list}}
-    </p>
     <button @click="getList()">一覧取得</button>
-  </div>
+      <ul> 
+        <li v-for="n of length" :key="n">
+          <Note v-bind:note="notes.data[n]"></Note>
+        </li>
+      </ul>
+    </div>
 </template>
 
 <script>
 import axios from 'axios'
+import Note from './components/Note.vue'
+
 export default {
+  name: 'App',
+  components: {
+    Note
+  },
   data(){
     return {
-      list: '',
+      notes: '',
+      length: '',
       title:'',
       content: ''
     }
@@ -26,11 +35,12 @@ methods: {
   getList(){
     axios.get('http://localhost:3000/api/v1/notes ')
     .then( response => {
-      this.list = response.data
+      this.notes = response.data
+      this.length = this.notes.data.length
     })
     .catch((err) => {
-      this.list = err
-    });
+      this.notes = err
+    })
   },
     postNote(title, content){
       axios.post('http://localhost:3000/api/v1/notes', {title: title, content: content, user_id:'1'})
@@ -39,7 +49,7 @@ methods: {
         response.data
       })
       .catch((err) => {
-        this.list = err
+        this.notes = err
       })
     }
   }
