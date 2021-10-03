3ac98b0404cc78a3ba5136584d0434a03bf958a9
Ran-Maru <63487316+Ran-Maru@users.noreply.github.com>
2021-10-03 09:01:21 +0900

    update メモ投稿処理を画面に実装

--- frontend/main_page/src/App.vue
+++ frontend/main_page/src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
+    タイトル
+    <input type="text" v-model='title'>
+    内容
+    <input type="text" v-model='content'>
+    <button @click='postNote(title, content)'>メモ作成</button>
     <p>
       {{list}}
     </p>
@@ -12,7 +17,9 @@ import axios from 'axios'
 export default {
   data(){
     return {
-      list: ''
+      list: '',
+      title:'',
+      content: ''
     }
   },
 methods: {
@@ -24,7 +31,17 @@ methods: {
     .catch((err) => {
       this.list = err
     });
-  }
+  },
+    postNote(title, content){
+      axios.post('http://localhost:3000/api/v1/notes', {title: title, content: content, user_id:'1'})
+      .then( response => {
+        // 適切な変数に代入する。
+        response.data
+      })
+      .catch((err) => {
+        this.list = err
+      })
+    }
   }
 }
 </script>
