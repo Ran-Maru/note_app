166f82cde6adde36fd428d9aac680634015ecfa2
Ran-Maru <63487316+Ran-Maru@users.noreply.github.com>
2021-10-12 08:06:55 +0900

    update App.vueからNoteList.vueを分離
    メモ一覧表示処理等を新規作成したNoteList.vue
    に移動

---
+++ frontend/main_page/src/components/NoteList.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    タイトル
+    <input type="text" v-model='title'>
+    内容
+    <input type="text" v-model='content'>
+    <button @click='postNote(title, content)'>メモ作成</button>
+    <button @click="getList()">一覧取得</button>
+      <ul> 
+        <li v-for="n of length" :key="n">
+          <NoteListItem v-bind:note="notes.data[n-1]"></NoteListItem>
+        </li>
+      </ul>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import NoteListItem from './NoteListItem.vue'
+
+export default {
+  name: 'NoteList',
+  components: {
+    NoteListItem
+  },
+  data(){
+    return {
+      notes: '',
+      length: '',
+      title:'',
+      content: ''
+    }
+  },
+methods: {
+  getList(){
+    axios.get('http://localhost:3000/api/v1/notes ')
+    .then( response => {
+      this.notes = response.data
+      this.length = this.notes.data.length
+    })
+    .catch((err) => {
+      this.notes = err
+    })
+  },
+    postNote(title, content){
+      axios.post('http://localhost:3000/api/v1/notes', {title: title, content: content, user_id:'1'})
+      .then( response => {
+        // 適切な変数に代入する。
+        response.data
+      })
+      .catch((err) => {
+        this.notes = err
+      })
+  },
+    throwAway(noteId){
+      axios.post('http://localhost:3000/api/v1/notes/trash', {id: noteId, user_id:'1'})
+      .catch((err) => {
+        this.err = err
+      })
+    },
+
+  },
+}
+</script>
