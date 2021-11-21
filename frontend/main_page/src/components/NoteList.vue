1d515de38a97ec13228ae78366b3b8507a87a98e
Ran-Maru <63487316+Ran-Maru@users.noreply.github.com>
2021-11-21 11:50:31 +0900

    update メモ新規作成フォームを別コンポーネントとして定義

--- frontend/main_page/src/components/NoteList.vue
+++ frontend/main_page/src/components/NoteList.vue
@@ -1,16 +1,6 @@
 <template>
   <div>
-    <button v-if="!postFormVisible" @click="toggleForm">メモを入力</button>
-    <div v-if="postFormVisible">
-      <input type="text" v-model='title' @keydown.enter="moveTab"
-        @compositionstart="composing=true"
-        @compositionend="composing=false"
-        placeholder="タイトル"
-      >
-      <textarea v-model='content' placeholder="メモを入力..."></textarea>
-      <button @click='postNote(title, content)'>メモ作成</button>
-      <button @click="toggleForm">保存せずに閉じる</button>
-    </div>
+      <NotePostField></NotePostField>
       <ul v-if="noteList" class="note-list"> 
         <li v-for="n of noteList.data.length" :key="n">
           <NoteListItem 
@@ -24,13 +14,14 @@
 
 <script>
 import axios from 'axios'
+import NotePostField from './NotePostField.vue'
 import NoteListItem from './NoteListItem.vue'
-import { ref }from 'vue'
 import { API } from '../const'
 
 export default {
   name: 'NoteList',
   components: {
+    NotePostField,
     NoteListItem
   },
 
@@ -39,40 +30,6 @@ export default {
     labelList:{}
   },
   setup(props){
-    let title = ref('')
-    let content = ref('')
-    let postFormVisible = ref(false)
-    // 日本語入力中(IME)を示すフラグ
-    let composing = ref(false)
-
-    // 表示・非表示を切り替える。
-    const toggleForm = () => {
-      postFormVisible.value = !postFormVisible.value
-      // 入力フォーム初期化
-      title.value = ''
-      content.value = ''
-    }
-
-    // 日本語入力中でない場合、エンター押下時にタブを移動させる。
-    const moveTab = () => {
-      if (composing.value){
-        return
-      }
-      const dom = document.activeElement
-      dom.nextElementSibling.focus()
-  }
-
-    const postNote = (title, content) => {
-      axios.post(API.NOTES, {title: title, content: content, user_id:'1'})
-      .then( response => {
-        // 適切な変数に代入する。
-        response.data
-      })
-      .catch((e) => {
-        this.value = e
-      })
-      toggleForm()
-    }
 
     const throwAway = (noteId) => {
       axios.post(API.NOTES_TRASH, {id: noteId, user_id:'1'})
@@ -83,13 +40,6 @@ export default {
     
     return {
       notes: props.notes,
-      title,
-      content,
-      postFormVisible,
-      composing,
-      toggleForm,
-      moveTab,
-      postNote,
       throwAway
     }
   }
