94dd39aca56d743dd80358cfd9ab32271c9f8efb
Ran-Maru <63487316+Ran-Maru@users.noreply.github.com>
2021-11-27 10:28:44 +0900

    update メインメニューのラベル名選択時にアクティブ状態になるよう修正

--- frontend/main_page/src/components/MainMenu.vue
+++ frontend/main_page/src/components/MainMenu.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
-    <nav class="nav nav-pills flex-column">
-      <a class="nav-link" href="#" @click="getNoteList()" id="notes-button">メモ</a>
+    <nav class="nav nav-pills flex-column" id="main-menu">
+      <a class="nav-link" href="#" @click="getNoteList($event)" id="notes-button">メモ</a>
       <a class="nav-link" href="#">リマインダー</a>
       <ul v-if="labelList" class="label-list">
         <li v-for="n of labelList.length" :key="n">
-          <a class="nav-link" href="#" @click="getLabeledNoteList(labelList[n-1].id)">
+          <a class="nav-link" href="#" @click="getLabeledNoteList(labelList[n-1].id, $event)">
             {{labelList[n-1].name}}
           </a>
         </li>
       </ul>
       <LabelEditDialog :labelList="labelList"></LabelEditDialog>
-      <a class="nav-link" href="#" @click="getArchivedList()">アーカイブ</a>
-      <a class="nav-link" href="#" @click="getTrashList()">ゴミ箱</a>
+      <a class="nav-link" href="#" @click="getArchivedList($event)">アーカイブ</a>
+      <a class="nav-link" href="#" @click="getTrashList($event)">ゴミ箱</a>
     </nav>
   </div>
 </template>
@@ -33,23 +33,44 @@ export default {
   emits: ['getNoteList'],
 
   setup(_, content){
-    const getNoteList = () => {
+    const getNoteList = ($event) => {
+      _activeNav($event)
       content.emit('getNoteList')
     }
 
-    const getLabeledNoteList = (labelId) => {
+    const getLabeledNoteList = (labelId, $event) => {
+      _activeNav($event)
       const params = {"label_id": labelId}
       content.emit('getNoteList', params)
     }
 
-    const getArchivedList = () => {
+    const getArchivedList = ($event) => {
+      _activeNav($event)
       const params = {"is_archived": true}
       content.emit('getNoteList', params)
     }
 
-    const getTrashList = () => {
+    const getTrashList = ($event) => {
+      _activeNav($event)
       const params = {"is_trash": true}
       content.emit('getNoteList', params)
+    }
+
+    // 選択されたnav要素にアクティブ状態のスタイルを設定する。
+    const _activeNav = ($event) => {
+      // アクティブ状態を初期化する。
+      // id="main-menu"のDOM内で、アクティブ状態になっているnav-linkのリストを取得する。
+      const nav = document.getElementById("main-menu")
+      const activeNavLinks = nav.getElementsByClassName('nav-link active');
+      // アクティブ状態になっているnavLinkがある場合、初期化処理をおこなう。
+      if (activeNavLinks.length > 0 ){
+        // アクティブ状態となるnav-linkは常に1つのため、0番目の要素にのみ処理をおこなう。
+        activeNavLinks[0].classList.remove("active")
+      }
+
+      // スタイルを設定する。
+      const navLink = $event.target
+      navLink.classList.add("active")
     }
 
     return{
