7d94e2f7b24bc7f2d09ad99bfd2cc942f31ee88d
Ran-Maru <63487316+Ran-Maru@users.noreply.github.com>
2022-01-15 14:16:39 +0900

    update App.vue構成見直し（その2）
    App.vueから各ページコンポーネントを呼び出す構成に変更

--- frontend/main_page/src/router.js
+++ frontend/main_page/src/router.js
@@ -1,12 +1,15 @@
 import SignUp from './components/SignUp.vue'
 import Login from './components/Login.vue'
+import Main from './components/Main.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
+      { path: '/', component: Main },
       { path: '/signUp', component: SignUp },
-      { path: '/login', component: Login }
+      { path: '/login', component: Login },
+      { path: '/main', component: Main }
   ]
 })
 
