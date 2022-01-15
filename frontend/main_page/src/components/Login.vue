33a3c48c41dd1ee6552f66c5d9477abc24fa60ea
Ran-Maru <63487316+Ran-Maru@users.noreply.github.com>
2022-01-15 14:56:11 +0900

    update ログイン画面に入力フォームを配置

--- frontend/main_page/src/components/Login.vue
+++ frontend/main_page/src/components/Login.vue
@@ -1,3 +1,30 @@
-<template>
-  <div>ログインのページ</div>
-</template>
+  <template>
+    <h1>ログインのページ</h1>
+    <input type="text" v-model="id" placeholder="ログインID">
+    <br>
+    <input type="password" v-model="password" placeholder="パスワード">
+    <br>
+    <button @click="login">ログイン</button>
+  </template>
+
+  <script>
+    import { ref }from 'vue'
+
+    export default {
+      name: 'Login',
+      setup(){
+        let id = ref('')
+        let password = ref('')
+
+        const login = () =>{
+          // ログインAPI実行
+          console.log(id.value + password.value)
+        }
+        return{
+          id,
+          password,
+          login
+        }
+      }
+    }
+  </script>
