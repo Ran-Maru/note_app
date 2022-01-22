72d9162a9ecc3546d228d10a4ba6a80d6f614e6d
Ran-Maru <63487316+Ran-Maru@users.noreply.github.com>
2022-01-22 11:52:21 +0900

    delet 誤って作成したマイグレーションファイルを削除
    テーブルのidカラムが自動作成されることを忘れ、作成したファイルのため削除する。

--- db/migrate/20210912122725_add_details_to_notes.rb
+++
@@ -1,6 +0,0 @@
-class AddDetailsToNotes < ActiveRecord::Migration[6.0]
-  def change
-    add_column :notes, :note_id, :string
-    change_column :notes, :note_id, :string, {null: false}
-  end
-end
